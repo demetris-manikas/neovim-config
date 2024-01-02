d59c87efe211ba9fcf0526b1d4f4a79a180d9155
Demetris Manikas <dmanikas@admin.grnet.gr>
2024-01-02 22:23:54 +0200

    minor fixes

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -3,9 +3,7 @@ return {
 	build = false,
 	cmd = "Spectre",
 	opts = { open_cmd = "noswapfile vnew" },
-    -- stylua: ignore
     keys = {
         { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-    }
-,
+    },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,8 +14,6 @@ vim.keymap.set("n", "<leader>wh", ":new <CR>", {})
 vim.keymap.set("n", "<C-s>", ":w <CR>", {})
 vim.keymap.set("n", "<C-z>", "u <CR>", {})
 
-local terminals = {}
-
 local function showLazyGit()
 	local cmd = "lazygit"
 	local opts = vim.tbl_deep_extend("force", {
@@ -23,30 +21,22 @@ local function showLazyGit()
 		size = { width = 0.9, height = 0.9 },
 	}, { persistent = true })
 
-	local termkey = vim.inspect({ cmd = cmd or "shell", cwd = opts.cwd, env = opts.env, count = vim.v.count1 })
-	if terminals[termkey] and terminals[termkey]:buf_valid() then
-		terminals[termkey]:toggle()
-	else
-		terminals[termkey] = require("lazy.util").float_term(cmd, opts)
-		local buf = terminals[termkey].buf
-		vim.b[buf].lazyterm_cmd = cmd
-		if opts.esc_esc == false then
-			vim.keymap.set("t", "<esc>", "<esc>", { buffer = buf, nowait = true })
-		end
-		if opts.ctrl_hjkl == false then
-			vim.keymap.set("t", "<c-h>", "<c-h>", { buffer = buf, nowait = true })
-			vim.keymap.set("t", "<c-j>", "<c-j>", { buffer = buf, nowait = true })
-			vim.keymap.set("t", "<c-k>", "<c-k>", { buffer = buf, nowait = true })
-			vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = buf, nowait = true })
-		end
-        vim.api.nvim_create_autocmd("BufEnter", {
-            buffer = buf,
-            callback = function()
-                vim.cmd.startinsert()
-            end,
-        })
-	end
-	return terminals[termkey]
+	local terminal = require("lazy.util").float_term(cmd, opts)
+	local buf = terminal.buf
+	vim.b[buf].lazyterm_cmd = cmd
+
+	vim.keymap.set("t", "<esc>", "<esc>", { buffer = buf, nowait = true })
+	vim.keymap.set("t", "<c-h>", "<c-h>", { buffer = buf, nowait = true })
+	vim.keymap.set("t", "<c-j>", "<c-j>", { buffer = buf, nowait = true })
+	vim.keymap.set("t", "<c-k>", "<c-k>", { buffer = buf, nowait = true })
+	vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = buf, nowait = true })
+
+	vim.api.nvim_create_autocmd("BufEnter", {
+		buffer = buf,
+		callback = function()
+			vim.cmd.startinsert()
+		end,
+	})
 end
 
-vim.keymap.set("n", "<leader>gg", showLazyGit, {})
+vim.keymap.set("n", "<leader>gg", showLazyGit, { desc = "Show lazygit" })
